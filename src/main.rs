1c0239ca1fd39b8e9c322ea4c0ba390adead1c36
degradka <degradka@gmail.com>
2023-01-22 15:29:24 +0300

    added loading screen

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use macroquad::logging as log;
 use macroquad::prelude::*;
-//use macroquad::texture;
-//use macroquad::{
-//    audio::{self, PlaySoundParams, Sound},
-//    experimental::coroutines::{start_coroutine, Coroutine},
-//};
+use macroquad::texture;
+use macroquad::{
+    audio::{self, PlaySoundParams, Sound},
+    experimental::coroutines::{start_coroutine, Coroutine},
+};
 use quad_rand as qrand;
+
+mod dispenser {
+    use std::any::Any;
+
+    static mut STORAGE: Option<Box<dyn Any>> = None;
+
+    pub fn store<T: Any>(data: T) {
+        unsafe {
+            STORAGE = Some(Box::new(data));
+        };
+    }
+
+    pub fn take<T: Any>() -> T {
+        unsafe { *STORAGE.take().unwrap().downcast::<T>().unwrap() }
+    }
+}
 
 const PROJECTION_WIDTH: f32 = 1600.0;
 const PROJECTION_HEIGHT: f32 = 900.0;
@@ -32,6 +48,36 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     log::debug!("Start game");
+    let loading_screen1 = load_texture("resources/system/images/loading-1.png").await.unwrap();
+    let loading_screen2 = load_texture("resources/system/images/loading-2.png").await.unwrap();
+    let loading_screen3 = load_texture("resources/system/images/loading-3.png").await.unwrap();
+    let background = load_texture("resources/system/images/intro.png").await.unwrap();
+
+    let resources_loading = start_coroutine(async move {
+        let tex_bunny = load_texture("resources/wabbit_alpha.png").await.unwrap();
+        dispenser::store(tex_bunny);
+    });
+
+    let mut current_loading_screen = 1;
+    while !resources_loading.is_done() {
+        clear_background(WHITE);
+        draw_texture(background, 0.0, 0.0, WHITE);
+        let (x, y) = (PROJECTION_WIDTH / 2.0, PROJECTION_HEIGHT / 2.0);
+        let (width, height) = match current_loading_screen {
+            1 => (loading_screen1.width(), loading_screen1.height()),
+            2 => (loading_screen2.width(), loading_screen2.height()),
+            _ => (loading_screen3.width(), loading_screen3.height()),
+        };
+        let (x, y) = (x - width / 2.0, y - height / 2.0);
+        match current_loading_screen {
+            1 => draw_texture(loading_screen1, x, y, WHITE),
+            2 => draw_texture(loading_screen2, x, y, WHITE),
+            _ => draw_texture(loading_screen3, x, y, WHITE),
+        }
+        current_loading_screen = (current_loading_screen % 3) + 1;
+        next_frame().await;
+    }
+    let tex_bunny = dispenser::take::<Texture2D>();
 
     let camera = macroquad::camera::Camera2D::from_display_rect(macroquad::math::Rect::new(
         0.0,
@@ -43,7 +89,6 @@ async fn main() {
 
     let mut bunnies = vec![Bunny { position: Vec2::new(0.0, 0.0), speed: Vec2::new(0.0, 0.0), color: WHITE }; MAX_BUNNIES];
     let mut bunnies_count = 0;
-    let tex_bunny = load_texture("resources/wabbit_alpha.png").await.unwrap();
 
     loop {
         if is_mouse_button_down(MouseButton::Left) {
